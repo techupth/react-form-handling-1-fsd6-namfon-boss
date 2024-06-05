909e59c55c5b9e22a1ef3045daa68c19cd5bc47e
Namfon <varisa.vb@hotmail.com>
2024-06-05 11:48:15 +0700

    feat(App.jsx): Add useState to onClick and onChange event

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
+  const [textInput, setInput] = useState("");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => setInput(event.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={() => setMessage(textInput)}>Update text</button>
       </div>
     </div>
   );
